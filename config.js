5d0ef8bef8a76b48b92b9c115661991f210f200b
Glitch (noiseless-exuberant-potato) <none>
2022-02-05 10:16:04 +0000

    Import of ssemihyilmaz/discord.js-v13-musicbot

---
+++ config.js
@@ -0,0 +1,21 @@
+module.exports = {
+        px: '!',
+        token: '', //DİSCORD BOTUNUZUN TOKENİNİ YAZIN
+        playing: 'Semih Yılmaz - Müzik Botu',
+
+    opt: {
+        DJ: {
+            enabled: false, //EĞER SADECE DJLER KULLANA BİLSİN İSTİYOR İSENİZ false yazanı true yapın.
+            roleName: 'DJ', //DJ ROLÜNÜN İSMİ NE OLACAK İSE YAZIN SUNUCUNUZDA O ROLDEKİLER KULLANA BİLİR
+            commands: ['back', 'clear', 'filter', 'loop', 'pause', 'resume', 'skip', 'stop', 'volume'] //DOKUNMA
+        },
+        maxVol: 250, //maximum ses seviyesi kaç olacak belirte bilirsiniz.
+        loopMessage: false, //elleme
+        discordPlayer: {
+            ytdlOptions: {
+                quality: 'highestaudio', //elleme
+                highWaterMark: 1 << 25 //elleme
+            }
+        }
+    }
+};
