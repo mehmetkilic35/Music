63e7203fa3fbc21144e1a67a51eca740faaef8ad
Glitch (flowmusic) <none>
2022-02-05 17:14:58 +0000

    🏗🏑 Checkpoint ./config.js:29016537/31

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
         px: '!', //prefix
-        playing: 'Semih Yılmaz-Müzik Botu', //Botunuzun oynuyor durumu
+        playing: '!h', //Botunuzun oynuyor durumu
 
     opt: {
         DJ: {
