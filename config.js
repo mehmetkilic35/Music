294957e6e0137413621da6b491c78a238e616834
Glitch (flowmusic) <none>
2022-05-17 20:15:08 +0000

    🍃🔋 Checkpoint ./config.js:10893506/96

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 module.exports = {
-        px: '!', //prefix
+        px: '!',
         playing: 'Youtube: AOPRAX',
         messageColor:'#FF6347',
 
     opt: {
         DJ: {
-            enabled: false, //Eğer sadece DJ'ler kullanabilsin istiyorsanız false yazanı true yapın. ~AOPRAX
-            roleName: 'DJ', //DJ rolünüzün ismi ne ise onu yazın. ~AOPRAX
+            enabled: false, // ~AOPRAX
+            roleName: 'DJ', //DJ rolünüzün ismi. ~AOPRAX
             commands: ['back', 'clear', 'filter', 'loop', 'pause', 'resume', 'skip', 'stop', 'volume']
         },
         maxVol: 250,
